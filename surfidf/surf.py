0a79a49e265cd2a2e78b9467eed4e0c77ff00393
Alex_Wang_LUT <severusmas@gmail.com>
2018-08-05 21:34:05 +0800

    changed print format

--- surfidf/surf.py
+++ surfidf/surf.py
@@ -9,7 +9,7 @@ import time
 time_start=time.time()
 # import file
 pipeline = import_file("source/dtest.*.dump")
-print("%s frame(s) in all" %pipeline.source.num_frames)
+print("%s frame(s) in all \nframe area vol vol_fraction" %pipeline.source.num_frames)
 # create list stepstr to save frame output
 cdt= [];
 #inti = frame+10
@@ -18,20 +18,19 @@ for frame in range(0, pipeline.source.num_frames, 1):
     data = pipeline.compute(frame)
     data.expect(SurfaceMesh)
     cell = data.expect(SimulationCell)
-    print("Surface area: %f" % data.attributes['ConstructSurfaceMesh.surface_area'])
-    print("Solid volume: %f" % data.attributes['ConstructSurfaceMesh.solid_volume'])
     fraction = data.attributes['ConstructSurfaceMesh.solid_volume'] / cell.volume
-    print("Solid volume fraction: %f" % fraction)
-    cmp = frame+1;
-    print('frame %s of %s complete' %(cmp,pipeline.source.num_frames))
-# convert list to numpy.array for plot and output
-    cdt.append(data.attributes['ConstructSurfaceMesh.surface_area'])
-surfid = array(cdt)
+    print("%s/%s %f %f %f" %(frame, pipeline.source.num_frames,\
+            data.attributes['ConstructSurfaceMesh.surface_area'],\
+            data.attributes['ConstructSurfaceMesh.solid_volume'],\
+            fraction))
+time_end = time.time()
+tsp = time_end - time_start
+print('OVER in %f ! └(^o^)┘ ' %tsp)
 
+# convert list to numpy.array for plot and output
+cdt.append(data.attributes['ConstructSurfaceMesh.surface_area'])
+surfid = array(cdt)
 np.savetxt("surface.txt", surfid, fmt="%2.3f", delimiter=" ")
-print('OVER ! └(^o^)┘ ')
-time_end=time.time()
-print('totally cost',time_end-time_start)
 pxx = range(np.size(surfid))
 plt.plot(pxx, surfid)
 plt.savefig('cracksurface.pdf')
