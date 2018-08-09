e22d47756b17e40d98e8cffbe86a5f8648cf762b
AlexWang0296 <severusmas@gmail.com>
2018-08-09 16:31:58 +0800

    no

--- surfidf/surf.py
+++ surfidf/surf.py
@@ -6,13 +6,13 @@ from ovito.io import import_file
 from ovito.data import SurfaceMesh, SimulationCell
 from ovito.modifiers import ConstructSurfaceModifier
 import time
-
-time_start=time.time()
+print(os.path.abspath(__file__))
+time_start = time.time()
 # import file
 pipeline = import_file('source/dtest.*.dump')
 print("%s frame(s) in all \nframe area vol vol_fraction" %pipeline.source.num_frames)
 # create list stepstr to save frame output
-cdt= [];
+cdt = []
 #inti = frame+10
 for frame in range(0, pipeline.source.num_frames, 1):
     pipeline.modifiers.append(ConstructSurfaceModifier(radius=2.9))
@@ -22,10 +22,10 @@ for frame in range(0, pipeline.source.num_frames, 1):
     fraction = data.attributes['ConstructSurfaceMesh.solid_volume'] / cell.volume
     cdt.append(data.attributes['ConstructSurfaceMesh.surface_area'])
 
-    print("%s/%s %f %f %f" %(frame, pipeline.source.num_frames,\
-            data.attributes['ConstructSurfaceMesh.surface_area'],\
-            data.attributes['ConstructSurfaceMesh.solid_volume'],\
-            fraction))
+    print("%s/%s %f %f %f" %(frame, pipeline.source.num_frames, \
+                             data.attributes['ConstructSurfaceMesh.surface_area'], \
+                             data.attributes['ConstructSurfaceMesh.solid_volume'], \
+                             fraction))
 time_end = time.time()
 tsp = time_end - time_start
 print('OVER in %f ! ^o^ ' %tsp)
@@ -35,8 +35,8 @@ surfid = np.array(cdt)
 np.savetxt("surface.txt", surfid, fmt="%2.3f", delimiter=" ")
 pxx = range(len(surfid))
 plt.plot(pxx, surfid)
-plt.savefig('cracksurface.pdf')
 plt.show()
-print(os.path.abspath(__file__))
+
+plt.savefig('cracksurface.pdf')
 
 
