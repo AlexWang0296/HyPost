a6f3dd24cebfdc5649f2d5ff260c735b79762730
alex0296 <severusmas@gmail.com>
2018-08-30 21:53:20 +0800

    prepared for hpc

--- dxa/dislen.py
+++ dxa/dislen.py
@@ -11,6 +11,8 @@ print(os.path.abspath(__file__))
 time_start = time.time()
 # import file
 fpath  = input("Path to Source File:\n")
+label = fpath.split('/')[1]
+#label = slice(fpath)[]
 pgap = int(input("Process every N Step:\n"))
 # /media/alex/HDD/post/ag/g-ag-300-opt/dump.*.cfg
 # 'source/dtest.*.dump'
@@ -27,7 +29,11 @@ for frame in range(0, pipeline.source.num_frames, pgap):
     vsolid = data.attributes['ConstructSurfaceMesh.solid_volume']
     cell = data.expect(SimulationCell)
     fraction = data.attributes['DislocationAnalysis.total_line_length'] / vsolid
+    with open('Dis_den_%s.txt'%label,'a+') as txt1:
+        txt1.writelines('%f\n'%data.attributes['DislocationAnalysis.total_line_length'])
     lenlist.append(data.attributes['DislocationAnalysis.total_line_length'])
+    with open('Dis_len%s.txt'%label,'a+') as txt2:
+        txt2.writelines('%f\n'%fraction)
     denlist.append(fraction)
     print("%s/%s %f  %f" %(frame, pipeline.source.num_frames,data.attributes['DislocationAnalysis.total_line_length'],fraction))
 time_end = time.time()
@@ -37,8 +43,8 @@ print('OVER in %f ! ^o^ ' %tsp)
 # convert list to numpy.array for plot and output
 dislength = np.array(lenlist)
 disdens = np.array(denlist)
-np.savetxt("Dis_length.txt", dislength, fmt="%2.3f", delimiter=" ")
-np.savetxt("Dis_density.txt", disdens, fmt="%2.3f", delimiter=" ")
+# np.savetxt("Dis_length.txt", dislength, fmt="%2.3f", delimiter=" ")
+# np.savetxt("Dis_density.txt", disdens, fmt="%2.3f", delimiter=" ")
 
 #pxx = range(len(dislength))
 #plt.plot(pxx, dislength)
